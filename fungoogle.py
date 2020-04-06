8c066109b8522f5fbcb157520d145eb9d000b0ae
Dipshi Shetty <2015dipshi.shetty@ves.ac.in>
2020-04-06 22:11:52 +0530

    changes n soup.select

--- fungoogle.py
+++ fungoogle.py
@@ -1,11 +1,13 @@
 import requests,sys,webbrowser,bs4
 
-res=requests.get('https://google.com/search?q='+''join(sys.argv[1:]))
+res=requests.get('https://google.com/search?q='+''.join(sys.argv[1:]))
 res.raise_for_status()
 
 soup=bs4.BeautifulSoup(res.text,"html.parser")
-linkElements = soup.select('.r a')
+print(soup)
+linkElements = soup.select('a[href^="/url?q="]')
+print(linkElements)
 linkToOpen = min(5,len(linkElements))
-
+print(linkToOpen)
 for i in range(linkToOpen):
     webbrowser.open('https://google.com'+linkElements[i].get('href'))
